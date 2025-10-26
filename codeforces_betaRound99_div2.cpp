19c39a844566ba743d923f7440a13817f7249fbe
Sandipan Ray <sray387.official@gmail.com>
2025-10-26 17:27:58 +0530

    Added

---
+++ codeforces_betaRound99_div2.cpp
@@ -0,0 +1,27 @@
+/*The program is the solution of codeforces problem no. A of codeforces beta round 99 (div 2)
+Name: Sandipan Ray
+Regn no.: 2025CA085
+Program Date: 26-10-2025    */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+int main () {
+    int n,count=0,sum=0,j=0;
+    cin >>n;
+    vector <int> vec(7);
+    for (int i=0; i<7; i++)
+        cin >> vec[i];
+    while (sum<n) {
+        sum+=vec[j];
+        count ++;
+        j++;
+        if (j==7)
+            j=0;
+    }
+    int final_answer=count %7;
+    if (final_answer==0)
+        cout << 7;
+    else cout << final_answer;
+    return 0;
+}
