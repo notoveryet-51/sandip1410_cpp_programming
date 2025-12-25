001fe476657d510efae47d8d9a463aa17d64915c
Sandipan Ray <sray387.official@gmail.com>
2025-12-25 17:17:38 +0530

    Added codeforces solutions

---
+++ C_The_Glitched_Nice_List_Calculator.cpp
@@ -0,0 +1,120 @@
+/*The program is the solution of problem
+Regn no.: 2025CA085
+Program Date: 25-12-2025    */
+
+/**
+ *  ███████╗ █████╗ ███╗   ██╗██████╗ ██╗██████╗  █████╗ ███╗   ██╗
+ *  ██╔════╝██╔══██╗████╗  ██║██╔══██╗██║██╔══██╗██╔══██╗████╗  ██║
+ *  ███████╗███████║██╔██╗ ██║██║  ██║██║██████╔╝███████║██╔██╗ ██║
+ *  ╚════██║██╔══██║██║╚██╗██║██║  ██║██║██╔═══╝ ██╔══██║██║╚██╗██║
+ *  ███████║██║  ██║██║ ╚████║██████╔╝██║██║     ██║  ██║██║ ╚████║
+ *  ╚══════╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═════╝ ╚═╝╚═╝     ╚═╝  ╚═╝╚═╝  ╚═══╝
+ *
+ *                 S A N D I P A N   R A Y
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+/* ---------- Macros ---------- */
+#define sza(x) ((int)(x).size())
+#define all(a) (a).begin(), (a).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+#define gcd(a, b) __gcd(a, b)
+
+/* ---------- Types ---------- */
+using ll = long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vpii = vector<pii>;
+using vpll = vector<pll>;
+using vc = vector<char>;
+using umii = unordered_map<int, int>;
+using umci = unordered_map<char, int>;
+using umsi = unordered_map<string, int>;
+using si = set<int>;
+using sc = set<char>;
+
+/* ---------- Constants ---------- */
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+const ld EPS = 1e-9;
+
+/* ---------- Math Utilities ---------- */
+ll lcm(ll a, ll b) { return (a / gcd(a, b)) * b; }
+
+ll mod_add(ll a, ll b, ll m = MOD) { return (a % m + b % m) % m; }
+ll mod_sub(ll a, ll b, ll m = MOD) { return ((a % m - b % m) + m) % m; }
+ll mod_mul(ll a, ll b, ll m = MOD) { return (a % m * b % m) % m; }
+
+ll mod_exp(ll a, ll b, ll m = MOD) {
+    ll res = 1;
+    while (b > 0) {
+        if (b & 1) res = mod_mul(res, a, m);
+        a = mod_mul(a, a, m);
+        b >>= 1;
+    }
+    return res;
+}
+
+ll binpow(ll b, ll n) {
+    ll result = 1;
+    while (n > 0) {
+        if (n & 1) result *= b;
+        b *= b;
+        n >>= 1;
+    }
+    return result;
+}
+
+bool prime(ll a) {
+    if (a <= 1) return false;
+    for (ll i = 2; i * i <= a; ++i)
+        if (a % i == 0) return false;
+    return true;
+}
+
+/* ---------- Output Helpers ---------- */
+inline void yes() { cout << "YES\n"; }
+inline void no()  { cout << "NO\n"; }
+
+/* ---------- IO Helpers ---------- */
+template <typename T>
+void read(vector<T>& v) { for (auto& x : v) cin >> x; }
+
+template <typename T>
+void print(const vector<T>& v) { for (const auto& x : v) cout << x << ' '; cout << '\n'; }
+
+/* ---------- Solve ---------- */
+void solve() {
+    string s1,s2;
+    cin >>s1>>s2;
+    string result="";
+    int n1=s1.size(),n2=s2.size(); 
+    for (int i=n1-1,j=n2-1; i>=0 || j>=0; i--,j--) {
+        int digit1=0,digit2=0;
+        if (i>=0) digit1=s1[i]-'0';
+        else digit1=0;
+        if (j>=0) digit2=s2[j]-'0';
+        else digit2=0;
+        result=to_string(digit1+digit2)+result;
+    }
+    cout<<result;
+}
+
+/* ---------- Main ---------- */
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+    return 0;
+}
---
+++ D_Santa_s_Magical_Numbers.cpp
@@ -0,0 +1,144 @@
+/*The program is the solution of problem
+Regn no.: 2025CA085
+Program Date: 25-12-2025    */
+
+/**
+ *  ███████╗ █████╗ ███╗   ██╗██████╗ ██╗██████╗  █████╗ ███╗   ██╗
+ *  ██╔════╝██╔══██╗████╗  ██║██╔══██╗██║██╔══██╗██╔══██╗████╗  ██║
+ *  ███████╗███████║██╔██╗ ██║██║  ██║██║██████╔╝███████║██╔██╗ ██║
+ *  ╚════██║██╔══██║██║╚██╗██║██║  ██║██║██╔═══╝ ██╔══██║██║╚██╗██║
+ *  ███████║██║  ██║██║ ╚████║██████╔╝██║██║     ██║  ██║██║ ╚████║
+ *  ╚══════╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═════╝ ╚═╝╚═╝     ╚═╝  ╚═╝╚═╝  ╚═══╝
+ *
+ *                 S A N D I P A N   R A Y
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+/* ---------- Macros ---------- */
+#define sza(x) ((int)(x).size())
+#define all(a) (a).begin(), (a).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+#define gcd(a, b) __gcd(a, b)
+
+/* ---------- Types ---------- */
+using ll = long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vpii = vector<pii>;
+using vpll = vector<pll>;
+using vc = vector<char>;
+using umii = unordered_map<int, int>;
+using umci = unordered_map<char, int>;
+using umsi = unordered_map<string, int>;
+using si = set<int>;
+using sc = set<char>;
+
+/* ---------- Constants ---------- */
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+const ld EPS = 1e-9;
+
+/* ---------- Math Utilities ---------- */
+ll lcm(ll a, ll b) { return (a / gcd(a, b)) * b; }
+
+ll mod_add(ll a, ll b, ll m = MOD) { return (a % m + b % m) % m; }
+ll mod_sub(ll a, ll b, ll m = MOD) { return ((a % m - b % m) + m) % m; }
+ll mod_mul(ll a, ll b, ll m = MOD) { return (a % m * b % m) % m; }
+
+ll mod_exp(ll a, ll b, ll m = MOD) {
+    ll res = 1;
+    while (b > 0) {
+        if (b & 1) res = mod_mul(res, a, m);
+        a = mod_mul(a, a, m);
+        b >>= 1;
+    }
+    return res;
+}
+
+ll binpow(ll b, ll n) {
+    ll result = 1;
+    while (n > 0) {
+        if (n & 1) result *= b;
+        b *= b;
+        n >>= 1;
+    }
+    return result;
+}
+
+bool prime(ll a) {
+    static const ll MAX = 1e6 + 5; 
+    static vector<bool> isPrime(MAX, true);
+    static bool initialized = false;
+    if (!initialized) {
+        isPrime[0] = isPrime[1] = false;
+        for (ll i = 2; i * i < MAX; ++i) {
+            if (isPrime[i]) {
+                for (ll j = i * i; j < MAX; j += i)
+                    isPrime[j] = false;
+            }
+        }
+        initialized = true;
+    }
+    if (a < 0 || a >= MAX) return false;
+    return isPrime[a];
+}
+
+/* ---------- Output Helpers ---------- */
+inline void yes() { cout << "YES\n"; }
+inline void no()  { cout << "NO\n"; }
+
+/* ---------- IO Helpers ---------- */
+template <typename T>
+void read(vector<T>& v) { for (auto& x : v) cin >> x; }
+
+template <typename T>
+void print(const vector<T>& v) { for (const auto& x : v) cout << x << ' '; cout << '\n'; }
+
+/* ---------- Solve ---------- */
+void solve() {
+    int n;
+    cin >>n;
+    int count=0;
+    if (n<4) {
+        no();
+        return;
+    }
+    if (n%2==0 && n>=4) {
+        count++;
+    }
+    else if (n%2!=0 && n>=5) {
+        if (prime(n-2))
+        count++;
+    }
+    if (n<=5) {
+        no();
+        return;
+    }
+    else count++;
+    if (count==2)   yes();
+    else no();
+    
+}
+
+/* ---------- Main ---------- */
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
